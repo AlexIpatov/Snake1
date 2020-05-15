ba963fb878fd90327c7ec7232cc73b235edf8b0c
AlexIpatov <alexspbperm@gmail.com>
2020-05-15 22:21:18 +0300

    Less_11

--- Snake1/Point.cs
+++ Snake1/Point.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.CSharp.RuntimeBinder;
 using System;
 using System.Collections.Generic;
+using System.Reflection.Metadata.Ecma335;
 using System.Text;
 
 namespace Snake1
@@ -21,12 +22,47 @@ namespace Snake1
             y = _y;
             sym = _sym;
         }
+        public Point( Point p)
+        {
+            x = p.x;
+            y = p.y;
+            sym = p.sym;
+        }
 
+        public void Move(int offset, Direction direction)
+        {
+            if(direction == Direction.RIGHT)
+            {
+                x = x + offset;
+            }
+            else if(direction == Direction.LEFT)
+            {
+                x = x - offset;
+            }
+            else if(direction == Direction.UP)
+                {
+                    y = y - offset;
+                }
+            else if (direction == Direction.DOWN)
+            {
+                y = y + offset;
+            }
+        }
 
         public void Draw()
         {
             Console.SetCursorPosition(x, y);
             Console.Write( sym );
+        }
+        public void Clear()
+        {
+            sym = ' ';
+            Draw();
+        }
+
+        public override string ToString()
+        {
+            return x + "," + y + "," + sym;
         }
     }
 }
--- Snake1/Program.cs
+++ Snake1/Program.cs
@@ -1,23 +1,48 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Threading;
 
 namespace Snake1
 {
     class Program
     {
         static void Main(string[] args)
+
         {
-            Point p1 = new Point(1, 3, '*');    
-            p1.Draw();
+            Console.SetWindowSize(80, 25);
+            Console.SetBufferSize(80, 25);
 
-            
-            Point p2 = new Point(4, 5, '#');
-            p2.Draw();
 
-            HorizontalLine line = new HorizontalLine(5, 10, 8, '+');
-            line.Drow();
+            HorizontalLine upLine = new HorizontalLine(0, 78, 0, '+');
+            HorizontalLine downLine = new HorizontalLine(0, 78, 24, '+');
+            VerticalLine leftLine = new VerticalLine(0, 24, 0, '+');
+            VerticalLine rightLine = new VerticalLine(0, 24, 78, '+');
+            upLine.Drow();
+            downLine.Drow();
+            rightLine.Drow();
+            leftLine.Drow();
 
-            Console.ReadLine();
+            Point p = new Point(4, 5, '*');
+            Snake snake = new Snake(p, 4, Direction.RIGHT );
+            snake.Drow();
+            snake.Move();
+
+            Console.CursorVisible = false;
+            while (true)
+            {
+                if(Console.KeyAvailable)
+                {
+                    ConsoleKeyInfo key = Console.ReadKey(true);
+                    snake.HandleKey(key.Key );
+
+
+                }
+                Thread.Sleep(100);
+                snake.Move();
+            }
+
+
+
         }
 
     }

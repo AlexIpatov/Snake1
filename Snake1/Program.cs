69331c311d191a3f2d5621ec5749276d4e184843
AlexIpatov <alexspbperm@gmail.com>
2020-05-15 19:04:08 +0300

    L_6

--- Snake1/Program.cs
+++ Snake1/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Snake1
 {
@@ -6,21 +7,16 @@ namespace Snake1
     {
         static void Main(string[] args)
         {
-            Point p1 = new Point();
-            p1.x = 1;
-            p1.y = 3;
-            p1.sym = '*';
+            Point p1 = new Point(1, 3, '*');    
             p1.Draw();
 
             
-            Point p2 = new Point();
-            p2.x = 4;
-            p2.y = 5;
-            p2.sym = '#';
+            Point p2 = new Point(4, 5, '#');
             p2.Draw();
 
+            
+
             Console.ReadLine();
-           
         }
 
     }

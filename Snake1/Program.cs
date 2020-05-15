64ebf2a85ee2dfba256f5c1900e589f52d4abc15
AlexIpatov <alexspbperm@gmail.com>
2020-05-15 19:23:39 +0300

    L_7

--- Snake1/Program.cs
+++ Snake1/Program.cs
@@ -14,7 +14,8 @@ namespace Snake1
             Point p2 = new Point(4, 5, '#');
             p2.Draw();
 
-            
+            HorizontalLine line = new HorizontalLine(5, 10, 8, '+');
+            line.Drow();
 
             Console.ReadLine();
         }
